f0bd2840f56a5b424ddb991f10ec6406e49ff68b
dsaikiran01 <dsaikiran559@gmail.com>
2025-03-27 01:02:46 +0530

    generator working with designs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,132 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react";
+
+const smallLetters = 'abcdefghijklmnopqrstuvwxyz';
+const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const digits = '0123456789';
+const specialChars = '@#$%^&*()_-+=/~';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [pswd, setPswd] = useState<string>('');
+  // const [isSmallLetter, setIsSmallLetter] = useState<boolean>(true);
+  const [isCapitalLetter, setIsCapitalLetter] = useState<boolean>(true);
+  const [isDigit, setIsDigit] = useState<boolean>(false);
+  const [isSpecialChar, setIsSpecialChar] = useState<boolean>(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [pswdLen, setPswdLen] = useState<number>(16);
+
+  const generatePswd = () => {
+    let dict = smallLetters;
+    if (isCapitalLetter) dict += capitalLetters;
+    if (isDigit) dict += digits;
+    if (isSpecialChar) dict += specialChars;
+
+    let pswdStr = '';
+    const dictLen = dict.length;
+    for (let i = 0; i < pswdLen; i++) {
+      let pos = Math.floor(Math.random() * dictLen);
+      pswdStr += dict[pos];
+    }
+
+    setPswd(pswdStr);
+  }
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(pswd)
+  }
+
+  useEffect(() => {
+    generatePswd();
+  }, [pswdLen, isCapitalLetter, isDigit, isSpecialChar]);
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center py-10 px-4">
+
+      {/* Navigation Section */}
+      <nav className="w-full text-center mb-6">
+        <h1 className="text-4xl font-semibold text-gray-800">Random Password Generator</h1>
+      </nav>
+
+      {/* Password Display Section */}
+      <div className="flex flex-col sm:flex-row items-center justify-between w-full max-w-2xl bg-white p-6 rounded-lg shadow-lg">
+        <input
+          className="text-center w-full sm:w-2/3 p-3 border-2 border-gray-300 rounded-md bg-gray-50 text-gray-600 font-medium"
+          value={pswd}
+          id="password"
+          disabled
+        />
+        <div className="flex space-x-4 mt-4 sm:mt-0">
+          <button
+            className="relative inline-flex items-center justify-center px-6 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 transition duration-300"
+            onClick={copyToClipboard}
+          >Copy</button>
+          <button
+            className="bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600 transition duration-300"
+            onClick={generatePswd}
+          >Load</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      {/* Range and Length Section */}
+      <div className="flex flex-col items-center mt-6 space-y-4 w-full max-w-md">
+        <div className="flex justify-between w-full px-6">
+          <span className="text-lg font-semibold text-gray-700">12</span>
+          <span className="text-lg font-semibold text-gray-700">72</span>
+        </div>
+        <input
+          className="w-full h-2 bg-blue-200 rounded-lg appearance-none cursor-pointer focus:outline-none"
+          type="range"
+          id="characterCount"
+          value={pswdLen}
+          min={12}
+          max={72}
+          step={1}
+          onChange={(e) => setPswdLen(Number(e.target.value))}
+        />
+      </div>
+
+      <div className="mt-6 w-full max-w-lg">
+        <ul className="flex flex-col sm:flex-row justify-around w-full space-y-4 sm:space-y-0 sm:space-x-4">
+          <li className="flex items-center space-x-2">
+            <input
+              className="h-5 w-5 text-blue-500"
+              type="checkbox"
+              checked
+              // onClick={() => setIsSmallLetter(prev => !prev)}
+              name="small-letters"
+            />
+            <label className="text-gray-700" htmlFor="small-letters">abc</label>
+          </li>
+          <li className="flex items-center space-x-2">
+            <input
+              className="h-5 w-5 text-blue-500"
+              type="checkbox"
+              defaultChecked={isCapitalLetter}
+              name="capital-letters"
+              onClick={() => setIsCapitalLetter(prev => !prev)}
+            />
+            <label className="text-gray-700" htmlFor="capital-letters">ABC</label>
+          </li>
+          <li className="flex items-center space-x-2">
+            <input
+              className="h-5 w-5 text-blue-500"
+              type="checkbox"
+              name="digits"
+              onClick={() => setIsDigit(prev => !prev)}
+            />
+            <label className="text-gray-700" htmlFor="digits">012</label>
+          </li>
+          <li className="flex items-center space-x-2">
+            <input
+              className="h-5 w-5 text-blue-500"
+              type="checkbox"
+              name="special-characters"
+              onClick={() => setIsSpecialChar(prev => !prev)}
+            />
+            <label className="text-gray-700" htmlFor="special-characters">$@&</label>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
